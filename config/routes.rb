93b4567083bb186ae7a61ea94f12c9fd526c212d
Pushkar Singh <theagilecoder@gmail.com>
2017-03-08 18:33:29 +0000

    Adds Shipments mvc

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :shipments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "shipments#index"
 end
